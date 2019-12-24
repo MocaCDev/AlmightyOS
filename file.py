5035f15094d10c3a6551379083f57345dc9532b1
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-24 14:49:29 -0600

    Create file.py

---
+++ file.py
@@ -0,0 +1,23 @@
+import os
+from files.server_setup import adapt
+
+main_dir = os.getcwd()
+os.chdir(main_dir)
+
+watch_for = [
+  'extras',
+  'Eject_Script',
+  'MiesPlatform',
+  'replace_shell',
+  '_Tool-Console_',
+  'NewJson'
+]
+
+append_found = []
+
+for i in range(len(os.listdir())):
+  if os.listdir()[i] in watch_for:
+    append_found.append(os.listdir()[i])
+
+adapt = adapt()
+adapt.gather_found_files(files=append_found)
