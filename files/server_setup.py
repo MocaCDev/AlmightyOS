46061ee9a57a1fd0c6f6de479ae5f27b1222e8d0
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-24 14:49:54 -0600

    Create server_setup.py

---
+++ files/server_setup.py
@@ -0,0 +1,16 @@
+import os, json
+
+class adapt:
+
+  def gather_found_files(self,**found_files):
+
+    "this will setup paths to found directories"
+    self.found_files = found_files['files']
+
+    "we want to replace / with ."
+    for i in range(len(self.found_files)):
+      self.found_files[i] = os.path.abspath(self.found_files[i]).replace('/','.')
+
+    with open('changed_paths.json','w') as file:
+      file.write(json.dumps(self.found_files,indent=2,sort_keys=False))
+      file.close()
